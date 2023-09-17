a9456a329e75119cc7f8b03786d5a30245fe52be
tommyiversonj <tommyiversonj@gmail.com>
2023-09-16 19:00:22 -0700

    Modified code

--- testing.c
+++ testing.c
@@ -6,95 +6,88 @@
 
 #define MAX_INPUT_SIZE 1024
 
-int main(int argc, char *argv[])
-	/*
-	 * main function starts from here
-	 */
+int main()
 {
-	char input[MAX_INPUT_SIZE];
-	int interactive = 1;
+    pid_t child_pid; // Declare child_pid here at the beginning of the block
+    char input[MAX_INPUT_SIZE];
+    int interactive = 1;
+
     /*
      * Assume interactive mode by default
      */
 
-	if (argc > 1)
-	{
-		/*
-		 * Check if any command-line arguments were passed
-		 */
-	interactive = 0;
-	}
-	
-	while (1)
-	{
-		if (interactive)
-		{
-			printf("($) "); 
-	    /*
-	     * Display a simple shell prompt
-	     */
-		fflush(stdout);
-	    /*
-	     * Flush the output buffer
-	     */
-		if (fgets(input, sizeof(input), stdin) == NULL)
-		{
+    while (1)
+    {
+        if (interactive)
+        {
+            printf("($) ");
+            /*
+             * Display a simple shell prompt
+             */
+            fflush(stdout);
+            /*
+             * Flush the output buffer
+             */
+            if (fgets(input, sizeof(input), stdin) == NULL)
+            {
                 perror("fgets");
                 exit(EXIT_FAILURE);
-		}
-	    }
-	    else
-	    {
-		if (fgets(input, sizeof(input), stdin) == NULL)
-		{
-			break;
-		/*
-		 * Exit the loop when there's no more input
-		 */
-		}
-	    }
+            }
+        }
+        else
+        {
+            if (fgets(input, sizeof(input), stdin) == NULL)
+            {
+                break;
+                /*
+                 * Exit the loop when there's no more input
+                 */
+            }
+        }
 
         /*
-	 * Remove newline character from input
-	 */
-	    input[strcspn(input, "\n")] = '\0';
+         * Remove newline character from input
+         */
+        input[strcspn(input, "\n")] = '\0';
 
         /*
-	 * Create a child process
-	 */
-	    pid_t child_pid = fork();
-	    
-	    if (child_pid == -1)
-	    {
-		perror("fork");
-		exit(EXIT_FAILURE);
-	    }
-	    
-	    if (child_pid == 0)
-	    {
+         * Create a child process
+         */
+        child_pid = fork();
+
+        if (child_pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+
+        if (child_pid == 0)
+        {
             /*
-	     * Child process
-	     * Execute the command entered by the user
-	     */
-		char *args[] = {"/bin/sh", "-c", input, NULL};
-		execvp("/bin/sh", args);
-		perror("execvp");
-	    /*
-	     * This line is reached if execvp fails
-	     */
-		exit(EXIT_FAILURE);
-	    } else
-	    {
+             * Child process
+             * Execute the command entered by the user
+             */
+            char *args_copy[] = {"/bin/sh", "-c", input, NULL};
+            execvp("/bin/sh", args_copy);
+            perror("execvp");
+
             /*
-	     * Parent process
-	     */
-		int status;
-		waitpid(child_pid, &status, 0);
-		if (WIFEXITED(status))
-		{
-			if (interactive)
-			{
-				printf("Child process exited with status %d\n", WEXITSTATUS(status));
+             * This line is reached if execvp fails
+             */
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            /*
+             * Parent process
+             */
+            int status;
+            waitpid(child_pid, &status, 0);
+            if (WIFEXITED(status))
+            {
+                if (interactive)
+                {
+                    printf("Child process exited with status %d\n", WEXITSTATUS(status));
                 }
             }
         }
